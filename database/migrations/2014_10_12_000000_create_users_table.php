2a276c533a39434076b9705bb8392834c1f36b4d
de Meeûs Augustin <gus@MacBook-Pro-de-de.local>
2021-05-23 21:52:11 +0200

    Final commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('api_token')->nullable()->default(null);
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->string('provider')->nullable();
             $table->string('provider_id')->nullable();
             $table->rememberToken();
